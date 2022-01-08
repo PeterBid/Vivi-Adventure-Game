eb732691ece9595178f00ccd77c918e8601272f1
Peter Bid <peterbid21@hotmail.com>
2022-01-08 15:14:48 +0000

    Cactus Obstacle made

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ function init() {
   const viviStartPosition = 94 //this starting position refers to an index
   let viviCurrentPosition = 94 //vivi’s current position refers to an index
 
+  //global variables for Cactus Obstacle
+  const cactusClass ='cactus' 
+  const cactusStartPosition = 89 //this starting position refers to an index
+  let cactusCurrentPosition = 89 //cactuars current position refers to an index
+
+
   //global variables for Topbar
   let score = 0
   let lives = 3
@@ -24,7 +30,6 @@ function init() {
   //--ELEMENT SELECTORS--//
 
   //element selectors for Topbar
-
   const selectReset = document.querySelector('#reset')
   const selectLives = document.querySelector('#lives')
   const selectScore = document.querySelector('#score')
@@ -50,13 +55,14 @@ function init() {
       cells.push(cell)
     }
     addVivi(viviStartPosition) // call the function to add the Vivi at the starting position
+    addCactus(cactusStartPosition) // call the function to add the Cactus at the starting position
   }
 
-  //--VIVI--//
+  //--Adding and Removing Vivi From Grid--//
 
   //adding Vivi to the grid
   function addVivi(position) { // this function is reusable for different positions
-    console.log('Position Being Passed In —>', position)
+    console.log('Position Being Passed In For Vivi —>', position)
     console.log('Cell Vivi picked Using The Position Index Passed In —>', cells[position])
     cells[position].classList.add(viviClass)
   }
@@ -66,6 +72,22 @@ function init() {
     cells[position].classList.remove(viviClass)
   }
 
+  //--Adding and Removing Obstacles From Grid--//
+  
+  //adding Cactus Obstacle to the grid
+  function addCactus(position) {
+    console.log('Cell Cactus picked Using The Position Index Passed In —>', cells[position])
+    cells[position].classList.add(cactusClass)
+  }
+
+  //removing Cactuar Obstacle from the grid
+  function removeCactus(position) {
+    cells[position].classList.remove(cactusClass)
+  }
+
+
+  //--Movment On The Grid--//
+
   //moving Vivi
   function handleKeyDown(event) {
     const key = event.keyCode
@@ -73,7 +95,7 @@ function init() {
     const right = 39
     const up = 38
     const down = 40
-    console.log('Position Before Redefining --->', viviCurrentPosition)
+    console.log('Position For Vivi Before Redefining --->', viviCurrentPosition)
     removeVivi(viviCurrentPosition)
 
     if (key === right && viviCurrentPosition % width !== width - 1) {
@@ -87,9 +109,12 @@ function init() {
     } else {
       console.log('Invalid Key')
     }
-    console.log('Position After Redefining --->', viviCurrentPosition)
+    console.log('Position For Vivi After Redefining --->', viviCurrentPosition)
     addVivi(viviCurrentPosition)
   }
+
+  //moving Cactus Obstacle
+  
 
 
   //Calling Event Listeners
